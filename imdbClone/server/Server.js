f54cbc225158e96491a1ad28efab61908eb9b4db
Aryan Naik <Aryan.22010128@viit.ac.in>
2023-07-28 13:57:57 +0530

    Initial commit

---
+++ imdbClone/server/Server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+require("./dbConnect");
+const mongoose = require("mongoose");
+const movie = require("./models/Movie");
+const express = require("express");
+
+const cors = require("cors")  ;  
+const app = express();
+const movieRoutes = require("./routes/Movies");
+ 
+
+
+
+
+const port = 5000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/your-database-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+app.use(express.json());
+
+// Use your routes
+app.use('/api', movieRoutes);
